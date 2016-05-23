6384d5d13f5830e998db4cef664c4ab3c2eb9060
Luc <l.nies@hotmail.com>
2016-05-23 11:53:28 +0200

    nodule reader fixes

--- src/read_nodule_patches.py
+++ src/read_nodule_patches.py
@@ -33,7 +33,7 @@ class NoduleReader:
     """
     Class balance = n_nodule_pixels/n_non_nodule_pixels
     """
-    def __init__(self, batch_size = 1100, shuffle = True, class_balance = 0.2, meta_data = 'image_stats.stat', label_path = lbl_path, patch_shape = (64,64)):
+    def __init__(self, batch_size = 1000, shuffle = True, class_balance = 0.8, meta_data = 'image_stats.stat', label_path = lbl_path, patch_shape = (64,64)):
         
         if not os.path.isfile(meta_data):
             preprocess.preprocess()
@@ -50,6 +50,7 @@ class NoduleReader:
         self.batch_size = batch_size
         self.label_path = label_path
         self.current = 0
+        self.current_slice = 0
         self.shuffle = shuffle
         self.patch_shape = patch_shape
         self.class_balance = class_balance
@@ -63,7 +64,7 @@ class NoduleReader:
     according to self.class_balance. Reapats this until n = batch_size patches are extracted
     """
     def next(self):
-        if self.current >self.n_samples-1:
+        if self.current > self.n_samples-1:
             raise StopIteration
         else:
 
@@ -72,31 +73,48 @@ class NoduleReader:
             patch_batch = np.zeros((self.batch_size, 1,) + self.patch_shape, dtype=np.float32)
             patch_labels = np.zeros((self.batch_size, 2), dtype=np.float32)
             
-            while n_batches < self.batch_size:
+            #while the batch size is not full and while there are subjects left
+            while n_batches < self.batch_size and self.current < self.n_samples:
 
                 batch, labels = load_subject(*self.get_locations())
                 batch = batch - self.mean
                  
-                
-                for i, label in enumerate(labels):
-                    print label.sum()
+                #loop over the slices starting from the current slice (0 if new subject, higher if batch was full before while there were still some slices left)
+                for i in np.arange(self.current_slice, len(labels)):
+                    #print i, label.sum()
+                    label = labels[i]
+                    
                     if label.sum() > 0: # so there is a nodule in the slice
-                        image_patches, image_labels = self.patch(batch[i], label, n_patches)
-                        patch_batch[n_batches*n_patches:n_batches+n_patches] = image_patches
-                        patch_labels[n_batches*n_patches:n_batches+n_patches] = image_labels
+                        #print n_batches, n_batches*n_patches+n_patches, self.batch_size
+                        image_patches, image_labels = self.patch(batch[i], label, n_patches) #the actual patching
+                        patch_batch[n_batches*n_patches:n_batches*n_patches+n_patches] = image_patches
+                        patch_labels[n_batches*n_patches:n_batches*n_patches+n_patches] = image_labels
                         n_batches += 1
+                        self.current_slice += 1 
                         
-                    self.current+=1
+                        #batch is completed but still in subject still has some slices left
+                        if n_batches*n_patches >= self.batch_size:
+                            
+                            #Shuffle batches
+                            indices = np.arange(self.batch_size)
+                            np.random.shuffle(indices)                            
+                            
+                            return patch_batch[indices], patch_labels[indices]
+                            
+                self.current_slice = 0
+                self.current+=1
                 
-                #If n_batch is not reached yet, but there are no more subjects
-                if self.current > self.n_samples-1:
-                    patch_batch = patch_batch[:n_batches]
-                    patch_labels = patch_labels[:n_batches]
+                
+                
+                    
                        
             
             
-            
-            
+            #If n_batch is not reached yet, but there are no more subjects
+            if self.current > self.n_samples-1:
+                patch_batch = patch_batch[:n_batches*n_patches]
+                patch_labels = patch_labels[:n_batches*n_patches]
+
             #Shuffle batches
             indices = np.arange(n_batches)
             np.random.shuffle(indices)
@@ -128,6 +146,7 @@ class NoduleReader:
         n_positives = min(len(pos_labels),n_patches*self.class_balance) #Not always enough positive labels, sometimes even 0.
         n_negatives = n_patches - n_positives #Are always more negative than positive labels
         
+        #randomly pick labels
         neg_labels =  np.random.choice(neg_labels, n_negatives, replace = False)
         pos_labels = np.random.choice(pos_labels, n_positives, replace = False)
         
@@ -141,17 +160,16 @@ class NoduleReader:
     
             patches[i, 0, :, :] = image[x:x+patch_size, y:y+patch_size]
             
-        patches = self.augment_patches(patches) #Augment all patches (both positive and negative samples)
+        patches = self.augment_patches(patches) #Augment all patches (both positive and negative samples, not really needed, but a lot of work to split this)
 
 
         return patches, patch_labels
 
     def get_locations(self):
-        
         image_location = self.file_names[self.current]
         split = image_location.split('/')
         label_location = os.path.join(self.label_path, (str(split[-1]) + '.npz')) 
-        #label_location = self.label_path + split[-1]  + '/'
+
         
         return image_location, label_location
     
@@ -160,7 +178,7 @@ class NoduleReader:
         augment_option = np.random.randint(0, 4, len(patches))
         for i,patch in enumerate(patches):
             if augment_option[i] == 1:#Transpose
-                patches[i] = patch.transpose()
+                patches[i][0] = patch[0].transpose()
             elif augment_option[i] == 2: #flip left-right
                 patches[i] = np.fliplr(patch)
             elif augment_option[i] == 3:#flip upside down
