c63ec46cebae21b9570839811ab4d5bfdba6bf37
Tom van de Poll <tomvandepoll@hotmail.com>
2016-05-26 22:21:48 +0200

    added recall calculation

--- src/evaluate.py
+++ src/evaluate.py
@@ -17,28 +17,55 @@ def load_itk_image(input_path):
     return image
 
 def dice_score_img(p,t):
-    return np.sum(p[t == 1]) * 2.0 / (np.sum(p) + np.sum(t))
+    sumpt = np.sum(p) + np.sum(t)
+    if sumpt == 0:
+        return 1.0
+    return (np.sum(p[t == 1.0]) * 2.0) / sumpt
+	
+def recall_img(p,t):
+    sumt = np.sum(t)
+    if sumt == 0:
+        return 1.0
+    return np.sum(p[t == 1.0])/ sumt
+
 
 if __name__ == "__main__":
     print "EVALUATING DICE SCORES\n======================"
     path = "../data/"
-    seg_path = "../data/seg-lungs-LUNA16/"
-    pred_path = "../data/pred/"
-    dices = np.empty(shape = (1))
+    seg_path = "I:/mevisnak/candidatemasks/candidates/"
+    pred_path = "I:/mevisnak/predictions/"
+    #dices = np.empty(shape = (len(os.listdir(pred_path))))
+    recalls = np.zeros(shape = (len(os.listdir(pred_path))/2))
     j = 0
-    for i,filename in enumerate(os.listdir(path)):
+    for i,filename in enumerate(os.listdir(pred_path)):
         if filename.endswith(".mhd"):
-            target = load_itk_image(seg_path+filename)
+            #target = load_itk_image(seg_path+filename)
+            target = np.load(seg_path+filename+".npz")
+            target = target['arr_0']*1.0
+            #print np.sum(target)
             prediction = load_itk_image(pred_path+filename)
             #print "Original images:\n  TARGET max: {0}, min: {1}, mean: {2}\n  PREDICTION: max: {3}, min: {4}, mean: {5}".format(np.max(target), np.min(target), np.mean(target), np.max(prediction), np.min(prediction), np.mean(prediction))
             target = stats.threshold(target, None, 0, 1)
             target = stats.threshold(target, 0, None, 0)
+            target *=1.0
+            #print np.sum(target)
             prediction = stats.threshold(prediction, None, 0, 1)
             prediction = stats.threshold(prediction, 0, None, 0)
+            #print prediction
             #print "After thresholding:\n  TARGET max: {0}, min: {1}, mean: {2}\n  PREDICTION: max: {3}, min: {4}, mean: {5}".format(np.max(target), np.min(target), np.mean(target), np.max(prediction), np.min(prediction), np.mean(prediction))
-            dices[j] = dice_score_img(prediction,target)
+            prediction *=1.0            
+            recalls[j] = recall_img(prediction, target)
+            print 'pred {0}, tar {1}, rec{2}'.format(np.sum(prediction), np.sum(target), recalls[j])
+            #dices[j] = dice_score_img(prediction,target)
+            #print np.sum(prediction*target)
             j += 1
-
-    mean = np.mean(dices)
-    std = np.std(dices)
-    print "Dice score mean {0}, std: {1}".format(mean, std) + "\n======================"
+            print '{0}\r'.format(j),
+    print recalls
+    #print dices
+    print
+    #mean = np.mean(dices)
+    #std = np.std(dices)
+    rmean = np.mean(recalls)
+    rstd = np.std(recalls)
+    #print "Dice score mean: {0}, std: {1}".format(mean, std) 
+    print "Recall mean: {0}, std: {1}".format(rmean, rstd) + "\n======================"
