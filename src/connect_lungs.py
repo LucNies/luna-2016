6dfb6b168c7a7d4cc11dfd738dc217c743d621cc
Tom van de Poll <tomvandepoll@hotmail.com>
2016-06-02 16:52:46 +0200

    added connected component filtering

--- src/connect_lungs.py
+++ src/connect_lungs.py
@@ -1,31 +1,47 @@
+from __future__ import division
 import os
 import numpy as np
 from scipy.ndimage import measurements
+import matplotlib.pyplot as plt 
+import evaluate
+from mpl_toolkits.mplot3d import Axes3D
 from tqdm import tqdm
 
 
 def filter_counts(counts):
-    return np.argsort(counts)[-2:] # Top 2
+    return np.argsort(counts)[-1] # Top 2
 
 
 def connect(img):
     mask, n = measurements.label(img)
-    counts = measurements.sum(img, mask)
+    print "TQDM"
+    counts = measurements.sum(img, mask, range(n))
+    print "TQDM.. more"
+    print "{}{}{}".format(len(counts), "/", n)
     lung_ids = filter_counts(counts)  # TODO: Pak alleen de longen
-    def f(v): return v in lung_ids
-    return f(mask)
-
+    print "TQDM thrice"
+    
+    return (mask == lung_ids).astype(int)
+    
 def rename(fn):
-    if "".endswith(".mhd.npz"):
+    if fn.endswith(".mhd.npz"):
         return "{}{}{}".format(fn[:-8], "_connected", fn[-8:])
     else:
         raise Exception("File name too complicated for me")
 
 
+def view(ndimg):
+    #s = ndimg[100]
+    for x in range(50,200,10):
+        plot = plt.imshow(ndimg[x])
+        plot.set_cmap('gray')
+        plt.show()
+
+
 if __name__ == "__main__":
     seg_dir = os.path.join("..", "segmentations", "nodules")
-    files = [os.path.join(seg_dir, f) for f in os.listdir(seg_dir)]
 
+    files = [os.path.join(seg_dir, f) for f in os.listdir(seg_dir)]
     def yield_predictions():
         for f in files:
             print "Processing {}".format(f)
@@ -33,3 +49,22 @@ if __name__ == "__main__":
 
     for fn, prediction in tqdm(yield_predictions()):
         np.savez_compressed(rename(fn), connect(prediction))
+    """
+    filename = files[125]
+    seg_path = "I:/mevisnak/truth/"
+    truth = evaluate.load_itk_image(seg_path+os.path.basename(filename)[:-4])
+    
+    seg = np.load(filename)['arr_0']*1
+    connected = connect(seg)
+    print set(truth.flatten())
+    print truth.mean()
+    print connected.mean()
+    print (truth == connected).sum()
+    print evaluate.dice_score_img(connected, truth >= 1)   
+    print evaluate.dice_score_img(seg, truth >= 1)    
+    
+    #view(connected)
+    #view(seg)
+    """
+  
+   
--- src/evaluate.py
+++ src/evaluate.py
@@ -10,6 +10,7 @@ import SimpleITK as sitk
 import numpy as np
 import scipy.stats as stats
 import os
+import connect_lungs
 from sklearn.metrics import confusion_matrix
 
 def load_itk_image(input_path):
@@ -21,7 +22,7 @@ def dice_score_img(p,t):
     sumpt = np.sum(p) + np.sum(t)
     if sumpt == 0:
         return 1.0
-    return (np.sum(p[t == 1.0]) * 2.0) / sumpt
+    return (np.sum(p[t == 1]) * 2.0) / sumpt
 
 def precision_img(p,t):
      conf = confusion_matrix(t, p, labels = [0,1])
@@ -40,46 +41,46 @@ def recall_img(p,t):
         return 1.0
     return np.sum(p[t == 1.0])/ sumt
     
-def evalute_lungs():
+def evaluate_lungs():
     print "EVALUATING DICE SCORES\n======================"
     path = "../data/"
-    seg_path = "I:/mevisnak/candidatemasks/candidates/"
-    pred_path = "I:/mevisnak/predictions/"
-    #dices = np.empty(shape = (len(os.listdir(pred_path))))
-    recalls = np.zeros(shape = (len(os.listdir(pred_path))/2))
+    seg_path = "I:/mevisnak/truth/"
+    pred_path = "I:/mevisnak/fcn_segs/lung_masks/"
+    dices = np.empty(shape = (len(os.listdir(pred_path))))
+    #recalls = np.zeros(shape = (len(os.listdir(pred_path))))
     j = 0
     for i,filename in enumerate(os.listdir(pred_path)):
-        if filename.endswith(".mhd"):
+        if filename.endswith(".npz"):
             #target = load_itk_image(seg_path+filename)
-            target = np.load(seg_path+filename+".npz")
-            target = target['arr_0']*1.0
+            target = np.load(pred_path+filename)
+            target = connect_lungs.connect(target['arr_0']*1.0)
             #print np.sum(target)
-            prediction = load_itk_image(pred_path+filename)
+            #prediction = np.load(pred_path+filename)
+            prediction = load_itk_image(seg_path+filename[:-4])
             #print "Original images:\n  TARGET max: {0}, min: {1}, mean: {2}\n  PREDICTION: max: {3}, min: {4}, mean: {5}".format(np.max(target), np.min(target), np.mean(target), np.max(prediction), np.min(prediction), np.mean(prediction))
             target = stats.threshold(target, None, 0, 1)
             target = stats.threshold(target, 0, None, 0)
-            target *=1.0
             #print np.sum(target)
             prediction = stats.threshold(prediction, None, 0, 1)
             prediction = stats.threshold(prediction, 0, None, 0)
             #print prediction
             #print "After thresholding:\n  TARGET max: {0}, min: {1}, mean: {2}\n  PREDICTION: max: {3}, min: {4}, mean: {5}".format(np.max(target), np.min(target), np.mean(target), np.max(prediction), np.min(prediction), np.mean(prediction))
             prediction *=1.0            
-            recalls[j] = recall_img(prediction, target)
-            print 'pred {0}, tar {1}, rec{2}'.format(np.sum(prediction), np.sum(target), recalls[j])
-            #dices[j] = dice_score_img(prediction,target)
+            #recalls[j] = recall_img(prediction, target)
+            #print 'pred {0}, tar {1}, rec{2}'.format(np.sum(prediction), np.sum(target), recalls[j])
+            dices[j] = dice_score_img(prediction,target)
             #print np.sum(prediction*target)
             j += 1
             print '{0}\r'.format(j),
-    print recalls
+    #print recalls
     #print dices
     print
-    #mean = np.mean(dices)
-    #std = np.std(dices)
-    rmean = np.mean(recalls)
-    rstd = np.std(recalls)
-    #print "Dice score mean: {0}, std: {1}".format(mean, std) 
-    print "Recall mean: {0}, std: {1}".format(rmean, rstd) + "\n======================"
+    mean = np.mean(dices)
+    std = np.std(dices)
+    #rmean = np.mean(recalls)
+    #rstd = np.std(recalls)
+    print "Dice score mean: {0}, std: {1}".format(mean, std) 
+    #print "Recall mean: {0}, std: {1}".format(rmean, rstd) + "\n======================"
 
 def evaluate_nodules(): 
     print "EVALUATING DICE SCORES\n======================"
@@ -132,4 +133,4 @@ def evaluate_nodules():
 
 
 if __name__ == "__main__":
-    evaluate_nodules()
+    evaluate_lungs()
